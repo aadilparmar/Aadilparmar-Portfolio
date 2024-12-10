8e47353f4af64718ae5c1e2a1a4bd2a9218137a5
aadilparmar <124196983+aadilparmar@users.noreply.github.com>
2024-12-10 14:35:32 +0530

    Second Commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx'
 import Navbar from './Sections/Navbar.jsx'
 import Hero from './Sections/Hero'
 import About from './Sections/About.jsx'
+import Projects from './Sections/Projects.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
     <Hero/>
     <About/>
-  </StrictMode>,
+    <Projects/>
+  </StrictMode>
 )
